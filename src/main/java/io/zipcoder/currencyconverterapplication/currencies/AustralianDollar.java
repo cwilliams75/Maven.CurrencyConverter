4705e3e556640bb02a1c8d738c7df573473cb039
cwilliams <coreywllms9@gmail.com>
2020-03-28 13:58:03 -0400

    currencies done

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
@@ -1,6 +1,14 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class AustralianDollar implements ConvertableCurrency {
+
+    ConvertableCurrency convertableCurrency;
+
+    @Override
+    public CurrencyType getCurrencyType(){
+        return CurrencyType.AUSTRALIAN_DOLLAR;
+    }
 }
